68c9ea83f3a31234cf83af2dda30ffcc66d7984c
Luo <gnulinux@126.com>
2016-06-14 16:56:02 +0800

    update

--- proxy/protocols/http/__init__.py
+++ proxy/protocols/http/__init__.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
 
+# HTTP 协议详解
+# http://a280606790.iteye.com/blog/1095085
+
 import methods  as Methods
 from  .request  import Request
 from  .response import Response
